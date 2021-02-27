101d6bb9c254c18a6f02ec4098572739b0440de5
Chilanka Halpage <59967984+Chilanka-Halpage@users.noreply.github.com>
2021-02-27 17:48:03 +0530

    Delete AmcCrudOperation directory

--- AmcCrudOperation/src/main/java/com/example/demo/Repository/TaxRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.entity.Tax;
-
-public interface TaxRepository extends JpaRepository<Tax,Integer>{
-
-}
