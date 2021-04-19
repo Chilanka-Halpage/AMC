eaa4b69f4250d2785b07144f67ca0ba5da954958
LaksithaKS <supunlaksitha96@gmail.com>
2021-04-19 20:05:30 +0530

    create notifications

--- Backend - Spring boot/src/main/java/com/itfac/amc/repository/LoginDtailsRepository.java
+++ Backend - Spring boot/src/main/java/com/itfac/amc/repository/LoginDtailsRepository.java
@@ -13,6 +13,6 @@ import com.itfac.amc.reportData.viewLoginDetails;
 public interface LoginDtailsRepository extends JpaRepository<LoginDetails, Integer> {
 
 	//void updateLastLoginDateForUserByName(String userName);
-	@Query(value = "Select u.user_id, u.uname, ld.loged_ip, ld.loged_datetime from user u, login_details ld where u.user_id = ld.client_id" , nativeQuery = true)
+	@Query(value = "Select u.user_id, u.uname, ld.loged_ip, ld.loged_datetime from user u, login_details ld where u.user_id = ld.user_id" , nativeQuery = true)
 	List<viewLoginDetails> loginDetails();
 }
